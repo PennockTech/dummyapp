4b209090bd3ef0b672b7a3df1855d2ce3d1600ea
Phil Pennock <phil@pennock-tech.com>
2018-02-22 20:45:55 -0500

    Bump in-code fallback version string to 0.0.2
    Add `-barebuild` suffix to indicate "not authoritative".

--- internal/version/version.go
+++ internal/version/version.go
@@ -12,8 +12,12 @@ import (
 
 const Program = "dummyapp"
 
-// overridden at link-time, if using Makefile
-var VersionString = "0.0.1"
+// VersionString is overridden at link-time, if using the Makefile.
+// If you see x.y.z output from the version command then that should mean
+// 'x.y.z'; if you see 'x.y.z-barebuild' then it's a hint that the version
+// number is merely whatever was in source, not authoritatively stamped in
+// later; this could thus be a build from any of a range of commits.
+var VersionString = "0.0.2-barebuild"
 
 // Pull the version derivation from whatever variables go into the makeup out
 // into a function so that we can log it at startup.
